7c186932379ff889a0f59d7ea1c7898ef2de53ca
jacinto <jacnto.gonzalez@gmail.com>
2024-02-01 17:47:01 -0600

    added eslint rules

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,15 @@
-import { Blogs } from "@/components/Blogs";
-import { Presentation } from "@/components/Presentation";
-import { getDataBlog } from "@/services/blogServices";
-import styles from "./page.module.css";
+import React from 'react'
+import { Blogs } from '@/components/Blogs'
+import { Presentation } from '@/components/Presentation'
+import { getDataBlog } from '@/services/blogServices'
+import styles from './page.module.css'
 
-export default async function Home() {
+export default async function Home () {
   const data = await getDataBlog()
   return (
     <main className={styles.main}>
       <Presentation/>
       <Blogs data={data}/>
     </main>
-  );
+  )
 }
